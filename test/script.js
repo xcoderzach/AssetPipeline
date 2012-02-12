e6e2f71929b7ebd142e42170044f2c95d1adbb2f
Zach Smith <x.coder.zach@gmail.com>
2012-02-12 16:27:28 -0600

    detect mimetypes and call next if we dont match the 'route'

--- test/script.js
+++ test/script.js
@@ -13,7 +13,7 @@ describe("AssetPipeline", function() {
   describe("loading a stylesheet", function() {
     it("should convert the .coffee to .js", function(done) {
       request.get('localhost:3001/modules/test.js', function(res) {
-        res.headers['content-type'].should.equal("text/javascript")
+        res.headers['content-type'].should.equal("application/javascript")
         res.text.should.equal(fs.readFileSync(__dirname + "/output/coffee_out.js", "utf8"))
         done()
       }) 
--- test/server.js
+++ test/server.js
@@ -5,12 +5,12 @@ var fs = require("fs")
   , testPipe = require("./pipes/test_pipe.js")
 
 server = connect(testPipe.server.middleware())
-server.listen(3001)
+server.listen(3003)
 
 describe("AssetServer", function() { 
   it("should serve the test file", function(done) {
     request.get('localhost:3001/modules/models/user.js', function(res) {
-      res.headers['content-type'].should.equal("text/javascript")
+      res.headers['content-type'].should.equal("application/javascript")
       res.text.should.equal(fs.readFileSync(__dirname + "/output/user_javascript_output.js", "utf8"))
       done()
     })
